cdfd1949af0e2b4afdbf9c99fa9b2ec447068816
RubenB25 <rubenbotello291@gmail.com>
2023-10-25 22:13:21 -0300

    Cambios en producto.html

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+
 const { createApp } = Vue;
-const app = createApp({
-  el: "#app",
+const app1 = createApp({
+  el: "#app1",
   data() {
     return {
       productos: [],
-      busqueda: "", 
+      busqueda: "",
+      productoSeleccionado: null,
     };
   },
   computed: {
@@ -13,10 +15,19 @@ const app = createApp({
       return this.productos
         .filter(producto => producto.name.toLowerCase().includes(busqueda))
         .slice(0, 3);
-    }
+    },
+    
+  },
+  methods: {
+    mostrarProducto(producto) {
+      const productoId = producto.id;
+          // Redirige a la página del producto con el ID
+          window.location.href = `producto.html?id=${productoId}`;
+   
+    },
   },
   created() {
-    const url = "https://653551d5c620ba9358ec623f.mockapi.io/moksha/products/"; // Reemplaza con la URL correcta de tu API
+    const url = "https://653551d5c620ba9358ec623f.mockapi.io/moksha/products/"; 
 
     fetch(url)
       .then((response) => {
@@ -32,7 +43,8 @@ const app = createApp({
         console.error(error);
       });
   },
-}); app.mount("#app")
+}); app1.mount("#app1")
+
 const menuBtn = document.getElementById('#menu-btn');
 const aContacto = document.querySelector('#a-contacto');
 const buscador = document.querySelector('#input-buscador');
@@ -112,3 +124,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
     });
 });
+const { createApp2 } = Vue;
+const app2 = createApp({
+    data() {
+        return {
+            producto: null, // Inicializar producto como nulo
+        };
+    },
+    created() {
+        // Obtener el ID del producto desde la URL
+        const params = new URLSearchParams(window.location.search);
+        const productoId = params.get("id");
+        const url = `https://653551d5c620ba9358ec623f.mockapi.io/moksha/products/${productoId}`;
+
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("No se pudo cargar los detalles del producto desde la API");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                this.producto = data; // Almacenar los detalles del producto
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    },
+});
+app2.mount("#app2");
