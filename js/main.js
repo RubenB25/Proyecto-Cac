1bcbaeea01819ecc1d6a520197ad94c561dfc9c2
RubenB25 <rubenbotello291@gmail.com>
2023-10-31 14:54:31 -0300

    Hombre y mujer agregado.

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+            url: './js/datos.json',
+            error: false,
+            datos: {}
+           
+        }
+    },
+    methods: {
+        fetchdata(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(  data => {
+                    console.log(data),
+                    this.datos=data
+
+                }
+                
+                
+                
+                );
+        }
+    },
+    created(){
+        this.fetchdata(this.url)
+
+    }
+
+}).mount('#app')
---
+++ js/main2.js
@@ -0,0 +1,32 @@
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+            url: './js/datos2.json',
+            error: false,
+            datos1: []
+           
+        }
+    },
+    methods: {
+        fetchdata(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(  data => {
+                    console.log(data),
+                    this.datos1=data;
+
+
+                }
+                
+                
+                
+                );
+        }
+    },
+    created(){
+        this.fetchdata(this.url)
+
+    }
+
+}).mount('#app')
