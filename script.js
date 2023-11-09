258344d3c8f705f215b009d975f965b818fb3129
ricardoluishernandez <ricardoluishernandez@gmail.com>
2023-11-09 12:13:32 -0300

    Modificar ingreso y registro
    Actualice el main con las modificaciones en el css para el registro y el ingreso y agregue las funciones de validacion para ambos formularios en JS

--- script.js
+++ script.js
@@ -90,3 +90,70 @@ document.addEventListener('DOMContentLoaded', () => {
 
     });
 });
+
+function validateForm() {
+    // Verificamos que el nombre de usuario no esté vacío.
+    if (document.getElementById("username").value === "") {
+      alert("Debe ingresar un nombre de usuario.");
+      return false;
+    }
+  
+    // Verificamos que la contraseña no esté vacía.
+    if (document.getElementById("password").value === "") {
+      alert("Debe ingresar una contraseña.");
+      return false;
+    }
+  
+    // La entrada es válida.
+    return true;
+  }
+ 
+  // Al hacer clic en el botón de envío, llamamos a la función de validación.
+  document.getElementById("submit").addEventListener("click", validateForm);
+
+  function validateReg() {
+    // Verificamos que todos los campos de formulario sean válidos.
+    if (document.getElementById("email").value === "") {
+      alert("Debe ingresar un email válido.");
+      return false;
+    }
+    
+    const email = document.getElementById("email").value;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+    if (!regex.test(email)) {
+      alert("Debe ingresar un email válido.");
+      return false;
+    }
+     
+    if (document.getElementById("usuario").value === "") {
+      alert("Debe ingresar un nombre de usuario.");
+      return false;
+    }
+  
+    if (document.getElementById("clave").value === "") {
+      alert("Debe ingresar una contraseña.");
+      return false;
+    }
+  
+    if (document.getElementById("confirmaClave").value === "") {
+      alert("Debe confirmar la contraseña.");
+      return false;
+    }
+  
+    if (document.getElementById("clave").value !== document.getElementById("confirmaClave").value) {
+      alert("Las contraseñas no coinciden. Por favor, inténtelo de nuevo.");
+      return false;
+    }
+  
+    // La entrada es válida.
+    return true;
+  }
+  
+  // Al hacer clic en el botón de envío, llamamos a la función de validación.
+  document.getElementById("submit").addEventListener("click", function() {
+    // Solo mostramos las alertas si el usuario hace clic en el botón de envío.
+    if (validateReg()) {
+      alert("Formulario enviado correctamente.");
+    }
+  });
